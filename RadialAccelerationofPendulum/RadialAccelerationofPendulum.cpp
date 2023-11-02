64a27e7ed970aca5e548d3bbff2b29953e4f99a8
Noordicsea <gtnoordsy24@icloud.com>
2023-11-02 08:37:41 -0500

    Added the code

--- RadialAccelerationofPendulum/RadialAccelerationofPendulum.cpp
+++ RadialAccelerationofPendulum/RadialAccelerationofPendulum.cpp
@@ -1,20 +1,47 @@
-// RadialAccelerationofPendulum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <cmath>
 
-#include <iostream>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main() {
+    double m, g, theta;
+    double T_horizontal, T_vertical, a_r;
+    char choice;
+
+    do {
+        // Input values
+        std::cout << "Enter the mass (m) in kg: ";
+        std::cin >> m;
+
+        std::cout << "Enter the gravitational acceleration (g) in m/s^2: ";
+        std::cin >> g;
+
+        std::cout << "Enter the angle (theta) in degrees: ";
+        std::cin >> theta;
+
+        // Convert theta to radians
+        theta = theta * M_PI / 180.0;
+
+        // Calculations
+        T_vertical = m * g;
+        T_horizontal = m * g * tan(theta);
+        a_r = T_horizontal / m;
+
+        // Display results
+        std::cout << "\nResults:\n";
+        std::cout << "Horizontal component of the force: " << T_horizontal << " N i-hat\n";
+        std::cout << "Vertical component of the force: " << T_vertical << " N j-hat\n";
+        std::cout << "Radial acceleration of the bob: " << a_r << " m/s^2\n";
+
+        // Ask user if they want to continue
+        std::cout << "\nDo you want to enter more variables for another problem? (y/n): ";
+        std::cin >> choice;
+        std::cout << "\n";
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
